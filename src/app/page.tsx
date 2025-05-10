0905e42db0493cae9c4660d011ef764ce9021853
yugantjha8 <74898454+yugantjha8@users.noreply.github.com>
2025-05-10 10:16:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -683,7 +683,7 @@ export default function Home() {
                 <p className="text-gray-600 mb-3">
                   We are located at
                 </p>
-                <<p className="text-blue-600 font-medium">
+                <p className="text-blue-600 font-medium">
                   SCO No 46, M3M Galleria Market, Sector 113, Dwarka Expressway
                 </p>
                 
